e00c10f4dc5aa79ed2cbd66d1670db2aedccc406
Will Gallagher <100447981+wgallagher44@users.noreply.github.com>
2023-06-24 14:09:50 -0400

    Drive Page

--- my_drive/src/Drive.jsx
+++ my_drive/src/Drive.jsx
@@ -1,8 +1,71 @@
-import React from "react";
-export default function Drive(){
-    return (
-    <div>
-       
-    </div>
-    )
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import { TextField } from '@mui/material';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import "./Drive.css"
+
+export default function Drive() {
+  
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    
+      <AppBar position="static">
+        <Toolbar id = "toolbar">
+            <div>
+                <TextField id = "search" label = "Search" variant="filled" color="success"  ></TextField>
+        <IconButton size="large" aria-label="search" color="inherit">
+            <SearchIcon />
+          </IconButton>
+            </div>
+            
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>View Profile</MenuItem>
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
+              </Menu>
+         
+          
+        </Toolbar>
+      </AppBar>
+   
+  );
 }
