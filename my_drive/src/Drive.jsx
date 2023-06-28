72fedb3f312304c9b0ab36df2dee7661942cde39
Will Gallagher <100447981+wgallagher44@users.noreply.github.com>
2023-06-28 12:01:39 -0400

    Drive Style

--- my_drive/src/Drive.jsx
+++ my_drive/src/Drive.jsx
@@ -7,13 +7,44 @@ import SearchIcon from '@mui/icons-material/Search';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
+import { DataGrid } from '@mui/x-data-grid';
 import "./Drive.css"
 
 export default function Drive() {
   
   const [anchorEl, setAnchorEl] = React.useState(null);
 
-
+  const rows = [
+    { id: 1, col1: 'Hello', col2: 'World' },
+    { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+    { id: 3, col1: 'MUI', col2: 'is Amazing' },
+    { id: 4, col1: 'Hello', col2: 'World' },
+    { id: 5, col1: 'DataGridPro', col2: 'is Awesome' },
+    { id: 6, col1: 'MUI', col2: 'is Amazing' },
+    { id: 1, col1: 'Hello', col2: 'World' },
+    { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+    { id: 3, col1: 'MUI', col2: 'is Amazing' },
+    { id: 4, col1: 'Hello', col2: 'World' },
+    { id: 5, col1: 'DataGridPro', col2: 'is Awesome' },
+    { id: 6, col1: 'MUI', col2: 'is Amazing' },
+    { id: 1, col1: 'Hello', col2: 'World' },
+    { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+    { id: 3, col1: 'MUI', col2: 'is Amazing' },
+    { id: 4, col1: 'Hello', col2: 'World' },
+    { id: 5, col1: 'DataGridPro', col2: 'is Awesome' },
+    { id: 6, col1: 'MUI', col2: 'is Amazing' },
+    { id: 1, col1: 'Hello', col2: 'World' },
+    { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+    { id: 3, col1: 'MUI', col2: 'is Amazing' },
+    { id: 4, col1: 'Hello', col2: 'World' },
+    { id: 5, col1: 'DataGridPro', col2: 'is Awesome' },
+    { id: 6, col1: 'MUI', col2: 'is Amazing' },
+  ];
+  
+  const columns = [
+    { field: 'col1', headerName: 'First Name'},
+    { field: 'col2', headerName: 'Last Name'},
+  ];
 
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
@@ -24,8 +55,8 @@ export default function Drive() {
   };
 
   return (
-    
-      <AppBar position="static">
+        <div>
+ <AppBar position="static">
         <Toolbar id = "toolbar">
             <div>
                 <TextField id = "search" label = "Search" variant="filled" color="success"  ></TextField>
@@ -66,6 +97,9 @@ export default function Drive() {
           
         </Toolbar>
       </AppBar>
+      <DataGrid rows={rows} columns={columns} checkboxSelection disableColumnMenu = "true" hideFooter = "true"/>
+        </div>
+     
    
   );
 }
